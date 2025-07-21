0351403055ee1849078d507d5a271d1f07f28d13
Dexter Sigurdson <dex@hermod.lan>
2025-07-20 23:59:43 -0500

    Added Banners in Footer

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/blog/posts/COPYRIGHT");
+  eleventyConfig.addPassthroughCopy("src/media");
 
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
